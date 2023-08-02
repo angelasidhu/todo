3e355995a3e0476fd55ce95a828048c4a0a46b23
Angela Sidhu <angela02sidhu@gmail.com>
2023-08-02 11:40:34 -0400

    complete iteration 2

--- todo/todoApp.swift
+++ todo/todoApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct todoApp: App {
+    let persistenceController = PersistenceController.shared
     var body: some Scene {
         WindowGroup {
-            ContentView()
-        }
+            ContentView().environment(\.managedObjectContext, persistenceController.container.viewContext)        }
     }
 }
