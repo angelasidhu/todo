4e3d8fdd0747b5a0777e5e9018e9d4dd1c5e3a4b
Angela Sidhu <angela02sidhu@gmail.com>
2023-08-02 09:29:38 -0400

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by scholar on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
